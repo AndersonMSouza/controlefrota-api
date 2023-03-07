0371e35cc48ae262f93252db916c2603c0a176c9
Anderson Mendes <mendes.ams@gmail.com>
2023-03-06 23:33:06 -0300

    Criação do projeto.

---
+++ src/main/java/com/mendes/anderson/ControlefrotaApiApplication.java
@@ -0,0 +1,13 @@
+package com.mendes.anderson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlefrotaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlefrotaApiApplication.class, args);
+	}
+
+}
