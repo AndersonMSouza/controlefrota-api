efc9b56e404315f6c375aed8daa637106c736778
Anderson Mendes <mendes.ams@gmail.com>
2023-03-07 21:34:06 -0300

    Criando a entidade Empresa.

---
+++ src/main/java/com/mendes/anderson/domain/model/Empresa.java
@@ -0,0 +1,42 @@
+package com.mendes.anderson.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Empresa {
+
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String cnpj;
+	
+	@Column(nullable = false)
+	private String razaoSocial;
+	
+	@Column(nullable = false)
+	private String nomeFantasia;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String telefone;
+	
+	@Column(nullable = false)
+	private String celular;
+	
+	@Column(nullable = false)
+	private String responsavel;
+	
+}
