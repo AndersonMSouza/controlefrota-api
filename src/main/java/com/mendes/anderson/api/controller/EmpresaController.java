28b704d13002732a895d4e842a1c3571210e5d88
Anderson Mendes <mendes.ams@gmail.com>
2023-03-10 23:35:37 -0300

    Criando método buscar.

--- src/main/java/com/mendes/anderson/api/controller/EmpresaController.java
+++ src/main/java/com/mendes/anderson/api/controller/EmpresaController.java
@@ -1,9 +1,12 @@
 package com.mendes.anderson.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,16 @@ public class EmpresaController {
 	@GetMapping
 	public List<Empresa> listar() {
 		return empresaRepository.findAll();
+	}
+	
+	public ResponseEntity<Empresa> buscar(@PathVariable Long empresaId) {
+		Optional<Empresa> empresa = empresaRepository.findById(empresaId);
+		
+		if (empresa.isPresent()) {
+			return ResponseEntity.ok(empresa.get());
+		}
+	
+		return ResponseEntity.notFound().build();		
 	}
 
 }
