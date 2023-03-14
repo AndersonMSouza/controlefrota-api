8f7ca9fa309b98ff01998bdcaa1d024363ebde46
Anderson Mendes <mendes.ams@gmail.com>
2023-03-14 20:23:22 -0300

    Criando método excluir e exception EntidadeEmUsoException.

---
+++ src/main/java/com/mendes/anderson/domain/exceptions/EntidadeEmUsoException.java
@@ -0,0 +1,11 @@
+package com.mendes.anderson.domain.exceptions;
+
+public class EntidadeEmUsoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EntidadeEmUsoException(String mensagem) {
+		super(mensagem);
+	}
+
+}
