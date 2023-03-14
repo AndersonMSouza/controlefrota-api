a7609ec57ee8ef63951d517f2caf3ae1ec0fcbc7
Anderson Mendes <mendes.ams@gmail.com>
2023-03-13 22:41:58 -0300

    Criando método atualizar.

--- src/main/java/com/mendes/anderson/api/controller/EmpresaController.java
+++ src/main/java/com/mendes/anderson/api/controller/EmpresaController.java
@@ -3,12 +3,14 @@ package com.mendes.anderson.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +50,27 @@ public class EmpresaController {
 		
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{empresaId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long empresaId,
+			@RequestBody Empresa empresa) {
+		try {			
+			Empresa empresaAtual = empresaRepository.findById(empresaId).orElse(null);
+			
+			if (empresaAtual != null) {
+				BeanUtils.copyProperties(empresa, empresaAtual, "id");
+				
+				empresaAtual = empresaRepository.save(empresaAtual);
+				return ResponseEntity.ok(empresaAtual);
+			}
+			
+			return ResponseEntity.notFound().build();
+		
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest()
+				.body(e.getMessage());
 		}
 	}
 }
