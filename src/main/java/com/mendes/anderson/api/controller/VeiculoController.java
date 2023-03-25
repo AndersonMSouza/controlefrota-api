adece1944a5ccb0baaeb618438331e5eece75428
Anderson Mendes <mendes.ams@gmail.com>
2023-03-24 23:08:15 -0300

    Criando método buscar.

--- src/main/java/com/mendes/anderson/api/controller/VeiculoController.java
+++ src/main/java/com/mendes/anderson/api/controller/VeiculoController.java
@@ -1,9 +1,12 @@
 package com.mendes.anderson.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +24,17 @@ public class VeiculoController {
 	public List<Veiculo> listar() {
 		return veiculoRepository.findAll();
 	}
+	
+	@GetMapping("/{veiculoId}")
+	public ResponseEntity<Veiculo> buscar(@PathVariable Long veiculoId) {
+		Optional<Veiculo> veiculo = veiculoRepository.findById(veiculoId);
+	
+		if (veiculo.isPresent()) {
+			return ResponseEntity.ok(veiculo.get()); 
+		}
+		
+		return ResponseEntity.notFound().build();
+	
+	}
+	
 }
