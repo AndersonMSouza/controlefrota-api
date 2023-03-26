29b0fa9a34946403a907771bb80429ebefbde2c7
Anderson Mendes <mendes.ams@gmail.com>
2023-03-25 23:28:42 -0300

    Criando método adicionar.

--- src/main/java/com/mendes/anderson/api/controller/VeiculoController.java
+++ src/main/java/com/mendes/anderson/api/controller/VeiculoController.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mendes.anderson.domain.exceptions.EntidadeNaoEncontradaException;
 import com.mendes.anderson.domain.model.Veiculo;
 import com.mendes.anderson.domain.repository.VeiculoRepository;
 
@@ -35,6 +39,17 @@ public class VeiculoController {
 		
 		return ResponseEntity.notFound().build();
 	
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> adicionar(@RequestBody Veiculo veiculo) {
+		try {
+			veiculo = veiculoRepository.save(veiculo);
+			return ResponseEntity.status(HttpStatus.CREATED).body(veiculo);
+		
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 	
 }
