eb6bbe3a7f0b005c9195ae1a740d0d01bfa742bc
Anderson Mendes <mendes.ams@gmail.com>
2023-03-27 22:42:04 -0300

    Criando método remover.

--- src/main/java/com/mendes/anderson/api/controller/VeiculoController.java
+++ src/main/java/com/mendes/anderson/api/controller/VeiculoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mendes.anderson.domain.exceptions.EntidadeEmUsoException;
 import com.mendes.anderson.domain.exceptions.EntidadeNaoEncontradaException;
 import com.mendes.anderson.domain.model.Veiculo;
 import com.mendes.anderson.domain.repository.VeiculoRepository;
@@ -70,5 +72,19 @@ public class VeiculoController {
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+	
+	@DeleteMapping("/{veiculoId}")
+	public ResponseEntity<Veiculo> remover(@PathVariable Long veiculoId) {
+		try {
+			veiculoRepository.deleteById(veiculoId);
+			return ResponseEntity.noContent().build();
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
+		
+		} catch (EntidadeEmUsoException e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}		
 	}
 }
