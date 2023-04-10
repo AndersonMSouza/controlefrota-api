7c263b04d3efeca448fd737ce510b6e3c9dd484b
Anderson Mendes <mendes.ams@gmail.com>
2023-04-10 17:54:18 -0300

    Corrigindo método salvar e criando método excluir da camada de serviço.

--- src/main/java/com/mendes/anderson/domain/service/CadastroPessoaService.java
+++ src/main/java/com/mendes/anderson/domain/service/CadastroPessoaService.java
@@ -1,19 +1,43 @@
 package com.mendes.anderson.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.mendes.anderson.domain.model.Veiculo;
-import com.mendes.anderson.domain.repository.VeiculoRepository;
+import com.mendes.anderson.domain.exceptions.EntidadeEmUsoException;
+import com.mendes.anderson.domain.exceptions.EntidadeNaoEncontradaException;
+import com.mendes.anderson.domain.model.Pessoa;
+import com.mendes.anderson.domain.repository.PessoaRepository;
 
 @Service
 public class CadastroPessoaService {
 	
-	@Autowired
-	private VeiculoRepository veiculoRepository;
+	private static final String MSG_PESSOA_EM_USO 
+	= "Pessoa de código %d não pode ser removida, pois está em uso!";
+
+	private static final String MSG_PESSOA_NAO_ENCONTRADA 
+	= "Não existe pessoa cadastrada com o código %d";
 	
-	public Veiculo salvar(Veiculo veiculo) {
-		return veiculoRepository.save(veiculo);
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa salvar(Pessoa pessoa) {
+		return pessoaRepository.save(pessoa);
+	}	
+	
+	public void excluir(Long pessoaId) {
+		try {
+			pessoaRepository.deleteById(pessoaId);
+		
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+				String.format(MSG_PESSOA_NAO_ENCONTRADA, pessoaId));
+		
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+				String.format(MSG_PESSOA_EM_USO, pessoaId));
+		}
 	}
 
 }
