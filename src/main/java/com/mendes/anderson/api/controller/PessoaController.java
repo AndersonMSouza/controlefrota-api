f05e76ccefc18cd4ef79f37cce81b7724c5e9446
Anderson Mendes <mendes.ams@gmail.com>
2023-04-13 19:26:47 -0300

    Refatorando método atualizar.

--- src/main/java/com/mendes/anderson/api/controller/PessoaController.java
+++ src/main/java/com/mendes/anderson/api/controller/PessoaController.java
@@ -1,7 +1,6 @@
 package com.mendes.anderson.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,21 +49,14 @@ public class PessoaController {
 	}
 	
 	@PutMapping("/{pessoaId}")
-	public ResponseEntity<?> atualizar(@PathVariable Long pessoaId, @RequestBody Pessoa pessoa) {
-		try {
-			Optional<Pessoa> pessoaAtual = pessoaRepository.findById(pessoaId);
+	public Pessoa atualizar(@PathVariable Long pessoaId,
+		@RequestBody Pessoa pessoa) {
+		
+		Pessoa pessoaAtual = pessoaRepository.findById(pessoaId).orElse(null);
 			
-			if (pessoaAtual.isPresent()) {
-				BeanUtils.copyProperties(pessoa, pessoaAtual.get(), "id");
-				Pessoa pessoaSalva = pessoaRepository.save(pessoaAtual.get());
-				return ResponseEntity.ok(pessoaSalva);
-			}
-			
-			return ResponseEntity.notFound().build();
-			
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
+		BeanUtils.copyProperties(pessoa, pessoaAtual, "id");
+		
+		return cadastroPessoaService.salvar(pessoaAtual);
 	}
 	
 	@DeleteMapping("/{pessoaId}")
